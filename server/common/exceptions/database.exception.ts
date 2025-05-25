19a98a4b6d2ceee873e460604f8c091dc5547f47
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-05-24 17:01:29 -0700

    Vamos desde 0
    Aprendí mucho en este semestre. Vamos a aplicar los conocimientos de NestJS

--- server/common/exceptions/database.exception.ts
+++
@@ -1,7 +0,0 @@
-export class DatabaseException extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'DatabaseException';
-        Object.setPrototypeOf(this, DatabaseException.prototype);
-    }
-}
--- server/common/interfaces/user.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import User from "../../user/user.entity"
-
-export default interface IUserRepository {
-    addUser(user: User): Promise<User>
-
-    getUserByEmail(email: string): Promise<User | null>
-}
